660b835cb55c452b1bf0b6150bb3a36dbaa56ec3
7THHYDRA <obaloluwa89@gmail.com>
2022-10-13 18:56:14 +0000

    2-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Serves as the entry point for the program
+ * Return: By default zero if no errors otherwise returns non-zero value.
+ */
+int main (void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+       return(0)
+}	       
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * *nmain - Entry Point
+ *
+ * REturn: Always 0 (Success)
+ */
+int main(void)
+{
+	return(0);
+}
