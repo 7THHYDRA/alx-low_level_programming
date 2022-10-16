367c3cc6d6a83c21668d89698bbb495ba82ba57e
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-16 10:42:29 +0000

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/**
- * main - entry point
+/*
+ * main - print a line of code using puts
  *
- * Return: Always 0
- */
+ * Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
