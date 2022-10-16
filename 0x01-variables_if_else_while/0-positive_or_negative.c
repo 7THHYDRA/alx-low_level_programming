206a91eb16351a10ada5e80a552a6c9b2416eb95
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-16 13:17:18 +0000

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Write a code that prints if a number is positive or negative
+ *
+ * Return - 0 to termintate the code
+*/
+int main(void)
+{
+	float num1;
+    printf("Test Number:");
+    scanf("%f",&num1);
+    if (num1<0)
+    {
+        printf("Negative");
+    }
+    else
+    {
+        if (num1>0);
+        printf("Positive");
+    }
+}
