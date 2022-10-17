6fc54c1fb78ad7040a9f224aa45b5e982db6a6fa
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-17 13:52:28 -0700

    All

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Anything.
+ *
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+	int row = 48;
+	int irow = row;
+	int column = 49;
+	int icolumn = column;
+
+	for (row = irow; row <= 56; row++)
+{
+	for (column = icolumn; column <= 57; column++)
+{	
+	putchar(row);
+	putchar(column);
+	if (row == '8' && column == '9')
+	break;
+	putchar(',');
+	putchar(' ');
+}
+	irow++;
+	icolumn++;
+}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Anything.
+ *
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	int row = 48;
+	int irow = row;
+	int column = 49;
+	int icolumn = column;
+
+	for (row = irow; row <= 56; row++)
+{
+	for (column = icolumn; column <= 57; column++)
+{
+	putchar(row);
+	putchar(column);
+	if (row == '8' && column == '9')
+	break;
+	putchar(',');
+	putchar(' ');
+}
+	irow++;
+	icolumn++;
+}
+	putchar('\n');
+	return (0);
+}
