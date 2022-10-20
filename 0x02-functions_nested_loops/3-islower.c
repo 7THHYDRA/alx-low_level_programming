4b264e34fd32843f35ba5d03a0177fde6a51da47
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-19 23:01:48 -0700

    KK

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check the code.
+ * @c: lower case character.
+ *
+ * Return: 1  as successful and return 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
