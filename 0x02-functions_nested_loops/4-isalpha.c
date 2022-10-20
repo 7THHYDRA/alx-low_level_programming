ec2bfcd7c4d3744d2e434836bd70c746b586e563
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-20 10:21:36 -0700

    GAGA

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,8 +8,14 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if (c >= 65 && c <= 122)
+{
 	return (1);
+}
+	else if (c >= 90 && c <= 97)
+{
+	return (0);
+}
 	else
 	return (0);
 }
