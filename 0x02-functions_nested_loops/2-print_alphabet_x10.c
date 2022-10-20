a9e3a9325f5dff3d24ea6f13aa87c09f8d5aa5dd
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-19 22:32:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,11 @@ void print_alphabet_x10(void)
 {
 	char i;
 	int N;
-	
+
 	for (N = 0; N <= 10; N++)
-{		
+{
 	for (i = 97; i <= 122; i++)
-	_putchar("%c", i);
+	_putchar(i);
 	_putchar("\n");
 }
-
 )
