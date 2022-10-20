27fe773794615cb7e94e166907f8ee9623473c2f
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-19 22:28:05 -0700

    KK

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - multiply 10 by alphabets.
+ *
+ * Return - da zero kpada..lol
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int N;
+	
+	for (N = 0; N <= 10; N++)
+{		
+	for (i = 97; i <= 122; i++)
+	_putchar("%c", i);
+	_putchar("\n");
+}
+
+)
