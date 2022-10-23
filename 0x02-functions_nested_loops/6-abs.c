ae6cfdc641338b572632d430f24c463f6b3d3a2d
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-23 09:02:00 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value computer.
+ * @c: natural value.
+ * Return: 0 as successful.
+ */
+
+int _abs(int c)
+{
+	if (c <= 0)
+	return (c);
+	else
+	return (-c);
+
+}
