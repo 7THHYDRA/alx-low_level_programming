ead7535d9b568315ca39d73564c545bf883cc81d
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-24 13:58:05 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset yo 98
+ * @n: integer to change its value or pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int c;
+	c = 98;
+	*n = c;
+}
