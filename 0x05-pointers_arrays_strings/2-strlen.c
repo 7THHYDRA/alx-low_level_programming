15bc3d42ef2737c67ccacdab59d331636a740dfa
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-25 07:45:21 -0700

    Oyinade

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - string lenght
+ * @s: s character
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	int j = 0;
+
+	while (*s != '\0')
+{
+	*s++;
+	j++;
+}
+	len = j;
+	return (len);
+}
