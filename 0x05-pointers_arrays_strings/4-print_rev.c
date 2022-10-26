7f054ec367309784c0d446a1d1b4c77e24b9effe
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-25 19:38:05 -0700

    aa

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,15 @@
 void print_rev(char *s)
 {
 	int i = 0;
+	int c = 0;
 
-	for (s[i] = 0; s[i] != '\0';)
+	for (s[i] = 0; s[i] !=  '\0'; i++)
 {
-	i++;
-	for (i = i - 1; i >= 0; i--)
-{
-	_putchar(s[i]);
+	c++;
 }
+	for (c = c - 1; c >= 0; c--)
+{
+	_putchar(s[c]);
 }
 	_putchar('\n');
 }
