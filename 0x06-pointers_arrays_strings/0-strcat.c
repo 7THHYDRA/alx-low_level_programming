a4c05a8369fd5b79c1fe2047c1fcdce44e57a0bd
7THHYDRA <Obaloluwa89@gmail.com>
2022-10-27 20:27:30 -0700

    aaaaaaHHH

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate
+ * @dest: char 1
+ * @src:
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+	*dest[a] = "";
+	*src[b] = "";
+
+	while (dest[a] = '\0')
+	dest['\0'] = src[0];
+	_putchar((dest + src));
+	return (dest);
+}
